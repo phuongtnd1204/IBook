e9974ea328a6a44cb0a3bef504f054bcde0ec58f
Quoc  Pham <phamxuandinhquoc@gmail.com>
2018-12-21 08:04:43 +0700

    test

--- IBook/IBook/IBook/ViewModels/AccountManagementViewModel.cs
+++ IBook/IBook/IBook/ViewModels/AccountManagementViewModel.cs
@@ -12,21 +12,12 @@ namespace IBook.ViewModels
         {
             userRepository = new UserRepository();
             GetUserList();
-<<<<<<< HEAD
         }
         public async void GetUserList()
         {
             UserList = new ObservableCollection<User>(await userRepository.ListAll());
         }
-=======
-;        }
->>>>>>> 45134c2f879e68129cd9fe22cac20f60e9dfae89
         private UserRepository userRepository { get; set; }
-
-        private async void GetUserList()
-        {
-            UserList =  new ObservableCollection<User>(await userRepository.ListAllAsync()) ;
-        }
         public ObservableCollection<User> UserList { get; set; }
     }
 }
